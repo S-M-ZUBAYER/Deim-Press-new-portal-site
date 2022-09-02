4942d75f390c509f532c76d27d597f5270207934
S-M-ZUBAYER <smzubayer18@gmail.com>
2022-09-03 00:00:30 +0600

    Show all news categoris in Dynamic way

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log('newsporter')
+
+// loadData from API server 
+const loadData = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await res.json();
+    showCategory(data.data.news_category);
+}
+loadData();
+
+// new menu 
+const showCategory = async (AllMenu) => {
+    const newsMenuField = document.getElementById('news-menu');
+    AllMenu.forEach(menu => {
+        const newsMenuDiv = document.createElement('div');
+        newsMenuDiv.classList.add('col');
+        newsMenuDiv.classList.add('py-3');
+        newsMenuDiv.innerHTML =
+            `
+        <h6>${menu.category_name ? menu.category_name : "No Name"}</h6>
+        `
+        newsMenuField.appendChild(newsMenuDiv);
+    });
+
+}
+
