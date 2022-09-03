a83d916ba447d507647acf57089e9f0d285154ef
S-M-ZUBAYER <smzubayer18@gmail.com>
2022-09-04 04:14:28 +0600

    change the icons and add some neccessary comments

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
-// loadData from API server 
+//<--------------- loadData from API server and check the error by using error handler --------->
 const loadData = async () => {
-
     try {
         const res = await fetch("https://openapi.programming-hero.com/api/news/categories");
         const data = await res.json();
@@ -9,11 +8,12 @@ const loadData = async () => {
     catch (error) {
         console.log(error);
     }
-
 }
+
+//<------- call the loadData function to show all news categories as a menubar ---------->
 loadData();
 
-// new menu 
+// <------------------show all news categories as a menubar----------------> 
 const showCategory = async (AllMenu) => {
     const newsMenuField = document.getElementById('news-menu');
     AllMenu.forEach(menu => {
@@ -26,10 +26,9 @@ const showCategory = async (AllMenu) => {
         `
         newsMenuField.appendChild(newsMenuDiv);
     });
-
 }
 
-// spinner part 
+//<------------- create a function to show the spinner when its needed ----------->
 const sppinerLoad = (isLoading) => {
     const sppinerField = document.getElementById('spinnerId');
     if (isLoading === true) {
@@ -41,7 +40,7 @@ const sppinerLoad = (isLoading) => {
 }
 
 
-// create a function for count 
+//<-----create a function for count the news and collect the data from API server and use error handler to check erro---->
 const countCategories = async (id, name) => {
     sppinerLoad(true);
     try {
@@ -63,8 +62,11 @@ const countCategories = async (id, name) => {
         console.log(error);
     }
 }
-countCategories("08");
-// display the news in card 
+
+//<------------ call the function to show the all news in initial period-------------> 
+countCategories("08", 'All News');
+
+//<-------- start the process to display the infomation about a news in a card and hide the blog section--------->
 const displayNews = (data) => {
     const displayField = document.getElementById('display-news');
     displayField.innerHTML = '';
@@ -73,7 +75,7 @@ const displayNews = (data) => {
     questionField.classList.add('d-none')
     sppinerLoad(false);
 
-    // check the length and show the notification data available or not
+    // <----------check the length and show the notification data available or not------->
     const notFound = document.getElementById('not-found');
     if (Allnews.length === 0) {
         notFound.classList.remove('d-none');
@@ -82,13 +84,15 @@ const displayNews = (data) => {
     else {
         notFound.classList.add('d-none');
     }
-    // sort all the arroy according the view numbers
+
+    // <---------sort all the objects in arroy according the viewer amount--------->
     const newarr = Allnews.sort(function (a, b) {
         return b.total_view - a.total_view
     });
 
+    // <---------- collect the particular data from API server and display each data in each card --------->
     Allnews.forEach(news => {
-        const { _id, total_view, title, image_url, details } = news;
+        const { _id, total_view, title, image_url, thumbnail_url, details } = news;
         const { name, published_date, img } = news.author;
         const cardField = document.createElement('div');
         cardField.innerHTML =
@@ -96,57 +100,34 @@ const displayNews = (data) => {
         <div onclick="showModel('${_id}')" class="card my-5 w-100 " data-bs-toggle="modal" data-bs-target="#newsInModal">
             <div class="row g-0 p-3" style="background-color: rgb(252, 222, 227)">
                 <div class="col-md-4 rounded">
-                    <img src="${image_url ? image_url : "Data Not Available"}" class="img-fluid rounded-start" alt="...">
+                    <img src="${image_url ? image_url : "Data Not Available"}" class="img-fluid h-100 w-full rounded-start" alt="...">
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
                         <h5 class="card-title fw-bold">${title ? title : "Data Not Available"}</h5>
                         <p class="card-text fw-semibold">${details.length > 350 ? details.slice(0, 350) + '...' : details}
-                        </p>
-                        <div class="container text-center ">
+                            </p>
+                        <div class="container ">
                             <div class="row ">
-                                <div class="col-4 d-flex align-items-end">
-                                    <div class="row text-center">
-                                        <div class="col-4 text-start"><img class="rounded-circle w-75 h-75" src="${img ? img : "Date Not Available"}<" alt=""></div>
+                                <div class="col-md-6 col-lg-4 ">
+                                    <div class="row text-center ">
+                                        <div class="col-4 text-start "><img class="rounded-circle w-75 h-75" src="${img ? img : "Date Not Available"}<" alt=""></div>
                                         <div class="col-8" style="margin-left: -30px;">
                                             <h6 class="mb-0">${name ? name : 'Date Not Available'}</h6>
                                             <p class="fs-6 mb-0">${published_date ? published_date : 'Date Not Available'}</p>
                                         </div>   
                                     </div>
                                 </div>
-                                <div class="col">${total_view ? total_view + "k" : "Data Not Available"}</div>
-                                <div class="col d-lg-block d-sm-none d-xs-none">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                                class="bi bi-star-fill text-warning" viewBox="0 0 16 16">
-                                <path
-                                    d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                                class="bi bi-star-fill text-warning" viewBox="0 0 16 16">
-                                <path
-                                    d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                                class="bi bi-star-fill text-warning" viewBox="0 0 16 16">
-                                <path
-                                    d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                                class="bi bi-star-fill text-warning" viewBox="0 0 16 16">
-                                <path
-                                    d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                            </svg>
-
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                                class="bi bi-star text-warning" viewBox="0 0 16 16">
-                                <path
-                                    d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
-                            </svg>
-                                </div>
-                                <div class="col text-end">
-                                <img src="https://cdn.onlinewebfonts.com/svg/img_96682.png" class="w-25 h-25" alt="...">
-                                <i class="fa-solid fa-arrow-right-from-arc"></i>
-                                
+                                <div class="col"><i class="fa-solid fa-eye pe-2"></i>${total_view ? total_view + "k" : "Data Not Available"}</div>
+                                <div class="col">
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-sharp fa-solid fa-star-half-stroke"></i>
+                                </div> 
+                                <div class="col">
+                                    <i class="fa-solid fa-arrow-right-long"></i>
                                 </div>
                             </div>
                         </div>
@@ -160,7 +141,7 @@ const displayNews = (data) => {
     })
 }
 
-// show the news details in modal 
+// <------collection of data from API server to provide in modal and check the error handler-------> 
 const showModel = async (news_id) => {
     try {
         const res = await fetch(`https://openapi.programming-hero.com/api/news/${news_id}`);
@@ -171,6 +152,8 @@ const showModel = async (news_id) => {
         console.log(error);
     }
 }
+
+// <---------------show the details information in a modal-----------------> 
 const modalDatadfd = (modalData) => {
     const questionField = document.getElementById('questions-field');
     questionField.classList.add('d-none')
@@ -193,7 +176,9 @@ const modalDatadfd = (modalData) => {
                      <div class="">${name ? name : "Data Not Available"}</div>
                 </div>    
             </div>
+            
             <div class="p-2 d-flex align-items-center">
+            <i class="fa-solid fa-eye pe-2"></i>
             ${total_view ? total_view + 'K' : "Data Not Available"}
             </div>
         </div>
@@ -203,6 +188,8 @@ const modalDatadfd = (modalData) => {
         `${published_date ? published_date : "Data Not Available"}`
 }
 
+
+// <----------conditional visibility of different questionnaire part----------->
 const showQuestionsAns = () => {
     const questionField = document.getElementById('questions-field');
     questionField.classList.remove('d-none')
